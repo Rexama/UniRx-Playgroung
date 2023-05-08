d69cfa769be42d5e79a0d9c8b97f81b3cf847a65
Rekin Barışıcı <rekinbar2000@gmail.com>
2023-05-08 11:52:23 +0300

    double click detector example

---
+++ Assets/_Examples/DoubleClickDetector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace _Examples
+{
+    public class DoubleClickDetector : MonoBehaviour
+    {
+        private void Start()
+        {
+            var clickStream = this.UpdateAsObservable().Where(_ => Input.GetMouseButtonDown(0));
+
+            clickStream.Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
+                .Where(x => x.Count >= 2)
+                .Subscribe(x => OnDoubleClick());
+        }
+        
+        private void OnDoubleClick()
+        {
+            Debug.Log("DoubleClick Detected!");
+        }
+    }
+}
